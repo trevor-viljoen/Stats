1ca4604f727205fbbea21ca0da90eadd40392345
digitalSaint <trevor.viljoen@gmail.com>
2015-02-12 00:03:19 -0600

    Fix for OS X date compatibility

--- Scoreboard/getScoreboardSchedule.sh
+++ Scoreboard/getScoreboardSchedule.sh
@@ -9,8 +9,13 @@ this_date=$start_reg_season
 
 until [ "$this_date" == "$end_reg_season" ]
 do
-  this_date=$(date --date="$this_date 1 day" +%Y%m%d)
   python getScoreboard.py $this_date
+  if [ "$(uname)" == "Darwin" ]
+  then
+    this_date=$(date -v+1d -jf %Y%m%d "${this_date}" +%Y%m%d)
+  else
+    this_date=$(date --date="$this_date 1 day" +%Y%m%d)
+  fi
   #   python getNCAAScoreboard.py $this_date
   #   python getGameXMLData.py "okst" $this_date
 done
