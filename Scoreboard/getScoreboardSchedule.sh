8352de779ad918a2d70df71dd6d1796a5e6f10c1
digitalSaint <trevor.viljoen@gmail.com>
2015-03-09 20:00:24 -0500

    If no commandline parameters are passed, the database is updated for the current day only.

--- Scoreboard/getScoreboardSchedule.sh
+++ Scoreboard/getScoreboardSchedule.sh
@@ -2,8 +2,14 @@
 
 #start_reg_season="20140214"
 #end_reg_season="20140525"
-start_reg_season="$1"
-end_reg_season="$2"
+if [ $? -lt 2 ]
+then
+  start_reg_season="$(date +%Y%m%d)"
+  end_reg_season="$start_reg_season"
+else
+  start_reg_season="$1"
+  end_reg_season="$2"
+fi
 
 this_date=$start_reg_season
 
